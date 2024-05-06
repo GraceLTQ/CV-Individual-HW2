84175735297893a7949dc9b5ec93ef548cc8252c
Grace <tl544@cornell.edu>
2024-05-06 15:37:10 -0400

    fliped but looks right

--- segmentation.py
+++ segmentation.py
@@ -5,49 +5,32 @@ def graph_based_segmentation(img):
     h, w, _ = np.shape(img)  # Height and width of the image
     N = h * w            # Total number of pixels
 
-    # Initialize a LIL matrix since it's efficient for constructing sparse matrices
-    W = np.zeros((N, N))
+    W = np.zeros((N, N))  # Weight matrix
     y1, x1 = np.unravel_index(np.arange(h*w), (h, w))
     y2, x2 = np.unravel_index(np.arange(h*w), (h, w))
 
-    # Iterate through all pairs of pixels within the defined neighborhood
-    for i in range(N):
-        py = y1[i]
-        px = x1[i]
-        for j in range(N):
-            qy = y2[j]
-            qx = x2[j]
-            if (py == qy and px == qx):
-                W[i, j] = 0
-            elif (np.abs(px - qx) <= 20) and (np.abs(py - qy) <= 20):
-                colorp = img[py, px, :]
-                colorq = img[qy, qx, :]
-                W[i][j] = np.exp(-100 * (np.linalg.norm(colorp - colorq))**2)
+    for r in range(N):
+        py, px = y1[r], x1[r]
+        for c in range(N):
+            qy, qx = y2[c], x2[c]
 
-    Denom = np.sum(W, axis=1)
+            if (px == qx and py == qy):
+                W[r, c] = 0
+            elif (np.abs(px - qx) <= 20 and np.abs(py - qy) <= 20):
+                color_p = img[py, px, :]
+                color_q = img[qy, qx, :]
+                W[r][c] = np.exp(-100 * (np.linalg.norm(color_p - color_q))**2)
 
-    # Construct the degree matrix D
+    denom = np.sum(W, axis=1)
     I = np.identity(N)
 
     for i in range(N):
-        # if Denom[i] != 0:
-        W[i, :] = W[i, :] / Denom[i]
+        W[i, :] = W[i, :] / denom[i]
 
-    # Construct the A matrix
     A = I - W
 
-    # Compute the eigenvalues and eigenvectors of the A matrix
     eigenvalues, eigenvectors = np.linalg.eig(A)
-
-    # The second eigenvector is the eigenvector associated with the second smallest eigenvalue
     second_eigenvalue = (np.argsort(eigenvalues))[1]
-
     second_eigenvector = eigenvectors[:, second_eigenvalue]
 
-    # Reshape the second eigenvector to match the original image dimensions before returning
     return np.reshape(second_eigenvector, (h, w))
-
-
-# Example usage:
-# img = np.array(Image.open('path_to_image.jpg'), dtype=np.float32) / 255.
-# segmentation_result = graph_based_segmentation(img)
